2e004252894a1c98c8eba00ed7cbb5c65ca4d099
jcoolstory <jcoolstory@gmail.com>
2016-12-07 13:39:51 +0900

    vector added

--- drawimage.ts
+++ drawimage.ts
@@ -19,30 +19,42 @@ function init(){
     }
     Resource.load();
 }
-
+var vector : Vector;
 function start(){
     var renderer = new Renderer();
     
     renderer.canvas = ctx;
     renderer.start();
-    for (var i = 0 ; i < 10 ; i++){
-        var x = Util.randomInt(width);
-        var y = Util.randomInt(height);
-        var testBody = new Body();
-        testBody.shape = new Rect(x,y,100,100);
-        testBody.debugging = true;
-        var image:any = Resource[imageUrl];    
-        testBody.image = new Bitmap(image);
-        testBody.render(ctx);
-        var moveX =  Math.random();
-        var moveY = Math.random();
-        testBody.move(moveX,moveY);
-        renderer.addObject(testBody);
-    }
+    // for (var i = 0 ; i < 10 ; i++){
+    //     var x = Util.randomInt(width);
+    //     var y = Util.randomInt(height);
+    //     var testBody = new Body();
+    //     testBody.shape = new Rect(x,y,100,100);
+    //     testBody.debugging = true;
+    //     var image:any = Resource[imageUrl];    
+    //     testBody.image = new Bitmap(image);
+    //     testBody.render(ctx);
+    //     var moveX =  Math.random();
+    //     var moveY = Math.random();
+    //     testBody.move(moveX,moveY);
+    //     renderer.addObject(testBody);
+    // }
 
+    var line = new LineBody(new Point(0,0), new Point(width/2, height/2));
     var wedge = new PoylgonBody();
     wedge.setPoints([10,10,30,10,40,20,50,150,20,60]);
-    renderer.addObject(wedge);
+
+    vector = new Vector(new Point(80,100),30,200);
+    vector.color = "#F00"
+   // vector.startRotate();
+    
+    var box = new Body();
+    box.color = "#00F";
+    box.shape = new Rect(50,100,800,50);
+    renderer.addObject(box);
+    renderer.addObject(vector);
+    // renderer.addObject(wedge);
+    // renderer.addObject(line);
 }
 
 class Canvas2D extends CanvasRenderingContext2D{
@@ -69,6 +81,15 @@ class Util {
     public static randomInt =  function(max:number) : number{
         return (Math.random() * max )| 0;
     }
+
+    public static toRadians = function(degrees) {
+        return degrees * Math.PI / 180;
+    };
+ 
+    // Converts from radians to degrees.
+    public static toDegrees = function(radians) {
+        return radians * 180 / Math.PI;
+    };
 }
 
 class MoveAnimate implements Animate {
@@ -151,8 +172,59 @@ class PoylgonBody implements RenderObject{
 
         if (this.closedPath)
             canvas.closePath();
+        canvas.fill();
+        canvas.stroke();
+    }
+}
+
+class Vector implements RenderObject{
+    position : Point = new Point();
+    angle : number=  0;
+    distance : number =1;
+    color : string = "#FFF";
+    constructor(point:Point, angle:number = 0, distance:number = 1){
+        this.position = point;
+        this.angle = angle;
+        this.distance = distance;
+    }
+    startRotate(){
+        var animate = new MoveAnimate();
+        animate.data = this;
+        var roff = Math.random()/2-0.5/2;
+        animate.callback = function(data:Vector){
+            data.angle +=roff;            
+        };
+        animate.start();
+    }
+    render(canvas:Canvas2D){
+        canvas.save();
+        canvas.strokeStyle = this.color;
+        canvas.beginPath();
+        canvas.moveTo(this.position.x, this.position.y);
+        var x = Math.cos(Util.toRadians(this.angle)) * this.distance;
+        var y = -Math.sin(Util.toRadians(this.angle)) * this.distance;
+        canvas.lineTo(this.position.x+x,this.position.y+y);
+        canvas.closePath();
         
         canvas.stroke();
+        canvas.fill();
+        canvas.restore();
+        canvas.fillStyle = "#FFF"
+        canvas.fillText((this.angle | 0).toString(),this.position.x -10, this.position.y-10);
+    }
+}
+class LineBody extends PoylgonBody{
+    public startPos : Point;
+    public endPos : Point;
+    constructor(start:Point,end:Point){
+        super()
+        this.startPos = start;
+        this.endPos = end;
+        this.updatePoint();
+    }
+
+    updatePoint(){
+        this.setPoints([this.startPos.x,this.startPos.y,this.endPos.x,this.endPos.y]);
     }
 }
 
@@ -172,6 +244,7 @@ class Renderer {
     }
 
     public render(canvas : Canvas2D){
+        canvas.lineWidth = 1;
         var oldfillStyle = canvas.fillStyle;
         canvas.fillStyle = this.backgroundColor;   
         canvas.fillRect(0,0,width,height);
@@ -192,6 +265,10 @@ class Renderer {
     public stop(){
         clearInterval(this.timer);
     }
+
+    public refresh(){
+        this.render(this.canvas);
+    }    
 }
 
 class Body implements RenderObject{
@@ -199,7 +276,7 @@ class Body implements RenderObject{
     image : Bitmap = null;
     debugging : Boolean = false;
     angle : number = 0;
-
+    color :string = "#000";
     move (x:number,y:number){
         var animate = new MoveAnimate();
         animate.data = this;
@@ -223,11 +300,15 @@ class Body implements RenderObject{
         canvas.rotate(this.angle);
         canvas.translate( -this.shape.width / 2 ,-this.shape.height/2)
         if (this.image)
+        {
             canvas.drawImage(this.image.source,this.image.x, this.image.y,this.image.width,this.image.height,0,0,this.shape.width,this.shape.height);
+        }
         else
+        {
+            canvas.strokeStyle = this.color;
             canvas.strokeRect(0,0,this.shape.width,this.shape.height);
-        if (this.debugging){
-            canvas.strokeStyle = "#fff";
+        }
+        if (this.debugging){            
             canvas.strokeRect(0,0,this.shape.width,this.shape.height);
         }
         canvas.restore();
@@ -277,3 +358,9 @@ class ResourceManager extends Object {
 }
 
 var Resource = new ResourceManager();
+
+
+function changeAngle(data){
+    var angle = parseFloat(data);
+    vector.angle = angle;
+}
