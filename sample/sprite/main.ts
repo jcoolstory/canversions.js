2f1bea58a2f42ae3b0627e8833371d09ff8c126a
jcoolstory <jcoolstory@gmail.com>
2017-01-04 19:09:33 +0900

    canversions rename

--- sample/sprite/main.ts
+++ sample/sprite/main.ts
@@ -6,9 +6,9 @@ enum Controlmode {
 
 class Tester {
     ctx :Canvas2D= null;
-    imageUrl1 = "./image/imagetest.png";
-    spriteImageUrl1 = "./image/spriteimage.png";
-    background = "./image/background.png";
+    imageUrl1 = "../../image/imagetest.png";
+    spriteImageUrl1 = "../../image/spriteimage.png";
+    background = "../../image/background.png";
     spriteBody :Cat ;
     vector : VectorBody;
     renderer: Renderer;
@@ -87,119 +87,39 @@ class Tester {
     }
     initBodies(){
         
-        var testBitmap = new Bitmap(Resource[this.imageUrl1]);
-        // for (var i = 0 ; i < 10 ; i++){
-        //     var x = MathUtil.randomInt( Resource.width);
-        //     var y = MathUtil.randomInt(Resource.height);
-        //     var testBody = new TestBody();
-        //     testBody.shape = new Rect(x,y,100,100);
-                
-        //     testBody.image =testBitmap;
-        //     testBody.render(this.ctx);
-        //     var moveX =  Math.random();
-        //     var moveY = Math.random();
-        //     testBody.move(moveX,moveY);
-        // }
-
+        
         var backgroundSprite = new ScrollSprite();
         backgroundSprite.image =  new Bitmap(Resource[this.background]);
         backgroundSprite.region = new Rect(0,0,Resource.width,Resource.height);
         var hratio =  backgroundSprite.image.height/ Resource.height  ;
         
         backgroundSprite.view = new Rect(0,0,backgroundSprite.image.width * hratio, backgroundSprite.image.height);
-        //this.renderer.addObject(backgroundSprite);
+        this.renderer.addObject(backgroundSprite);
 
+
+        var testBitmap = new Bitmap(Resource[this.imageUrl1]);
+        for (var i = 0 ; i < 10 ; i++){
+            var x = MathUtil.randomInt( Resource.width);
+            var y = MathUtil.randomInt(Resource.height);
+            var testBody = new TestBody();
+            testBody.shape = new Rect(x,y,100,100);
+                
+            testBody.image =testBitmap;
+            testBody.render(this.ctx);
+            var moveX =  Math.random();
+            var moveY = Math.random();
+            testBody.move(moveX,moveY);
+            this.renderer.addObject(testBody);
+        }
 
         var image1 : any =  Resource[this.spriteImageUrl1];
         var spriteImage = new SpriteBitmap(image1,[new Rect(0,0,512,256),new Rect(512,0,512,256), new Rect(0,256,512,256),new Rect(512,256,512,256),new Rect(0,512,512,256),new Rect(512,512,512,256)])
         this.spriteBody = new Cat();
         this.spriteBody.image = spriteImage;
-        this.spriteBody.shape = new Rect(400,400,256,128);
-    // renderer.addObject(spriteBody);
-        var line = new LineBody(new Point(0,0), new Point(Resource.width/2, Resource.height/2));
-        var wedge = new PolygonBody();
-        
-        wedge.setPoints([500,300,600,300,600,200,400,200,500,300]);    
-
+        this.spriteBody.shape = new Rect(100,300,256,128);
+        this.renderer.addObject(this.spriteBody);        
         
         
-        var box = new TestBody();
-        box.color = "#00F";
-        box.shape = new Rect(307,316,500,300);
-        this.renderer.addObject(box);
-        //this.vector = new VectorBody(new Point(200,200),-30,5000);
-        var anlearray = [-30,30,45,90,100,140,200,240,300,330]
-        var relationBody : Body[]= [box,wedge];
-        for (var i= 0 ; i < 10; i++){
-            var vector= new VectorBody(new Point(200,200),anlearray[i],500);
-            var polygon = new RayCastVectorBody();
-            polygon.vector = vector;
-            polygon.relationBody = relationBody;
-           this.renderer.addObject(polygon);
-            vector.startRotate();
-        }
-
-          var vector1= new VectorBody(new Point(500,400),140,200);
-            var polygon1 = new RayCastVectorBody();
-            polygon1.vector = vector1;
-            polygon1.relationBody = relationBody;
-            this.renderer.addObject(polygon1);
-            //vector.startRotate();
-
-        this.polygon = polygon;
-
-        //this.vector.startRotate();
-        //renderer.addObject(vector);
-        this.renderer.addObject(wedge);
-        // renderer.addObject(line);
-        
-        //var PolygonBody : PolygonBody = new PolygonBody
-
-        //renderer.refresh();
-
-        var circleBody  = new CircleActor();
-        circleBody.shape.x = 500;
-        circleBody.shape.y = 400;
-        circleBody.shape.width = 5;        
-        circleBody.color = "red";
-        circleBody.relationBody = relationBody;
-        circleBody.move(5,-2);
-
-        var circleBody1 = new CircleBody();
-        circleBody1.shape.x = 500;
-        circleBody1.shape.y = 100;
-        circleBody1.shape.width = 50;
-        circleBody1.color= "blue";
-        relationBody.push(circleBody1);
-        this.renderer.addObject(circleBody1)
-
-        var lines = new LineBody(new Point(102,102), new Point(105,170));
-        lines.color = "red";
-        this.renderer.addObject(lines);
-        //this.renderer.addObject(circleBody);
-        
-        var points =  MathUtil.circlelineintersection(new Point(circleBody1.shape.x,circleBody1.shape.y), circleBody1.shape.width,lines.startPos,lines.endPos);
-
-        for( var i = 0 ; i < points.length; i++){
-            var vertex1 = new CircleBody();
-            vertex1.shape.x = points[i].x;
-            vertex1.shape.y = points[i].y;
-            vertex1.shape.width = 1;
-            vertex1.color = "yellow"    
-            this.renderer.addObject(vertex1);
-        }
-
-
-        var line2 = new LineBody(new Point(200,200), new Point(225,125));
-        line2.color = "yellow";
-
-        var vertex3 = new CircleBody();
-        vertex3.shape.x = 200;
-        vertex3.shape.y = 10;
-        vertex3.shape.width = 1;
-        vertex3.color = "yellow";
-        this.renderer.addObject(line2);
-        this.renderer.addObject(vertex3);
         document.addEventListener("keydown",this.OnKeyDown);
 
         this.start();
